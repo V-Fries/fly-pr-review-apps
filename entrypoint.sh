e14c0279889279ed18bd7f6cd32ab23b14cb5b6a
Vincent Fries <91223304+V-Fries@users.noreply.github.com>
2025-03-12 05:42:39 +0100

    Automatically make app name uppercase chars lowercase

--- entrypoint.sh
+++ entrypoint.sh
@@ -20,6 +20,7 @@ EVENT_TYPE=$(jq -r .action /github/workflow/event.json)
 app="${INPUT_NAME:-pr-$PR_NUMBER-$GITHUB_REPOSITORY_OWNER-$GITHUB_REPOSITORY_NAME}"
 # Change underscores to hyphens.
 app="${app//_/-}"
+app="$(echo "$app" | tr '[:upper:]' '[:lower:]')"
 region="${INPUT_REGION:-${FLY_REGION:-iad}}"
 org="${INPUT_ORG:-${FLY_ORG:-personal}}"
 image="$INPUT_IMAGE"
